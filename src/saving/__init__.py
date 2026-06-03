fa5456e9690b87b4987ec8861c7618621d93daef
TommyVaninetti <tommasovaninetti8@gmail.com>
2026-06-03 21:22:18 +0200

    added AGPLv3 header to all files

--- src/saving/__init__.py
+++ src/saving/__init__.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2026 Tommaso Vaninetti
+#
+# This file is part of PlantLeaf.
+#
+# PlantLeaf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# PlantLeaf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PlantLeaf. If not, see <https://www.gnu.org/licenses/>.
+
 from .voltage_save_worker import VoltageSaveWorker  
 from .audio_save_worker import AudioSaveWorker
 from .audio_load_progress import AudioLoadWorker  
